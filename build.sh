9773778a7f54426f546a2e45a12dd5cf20ecaed1
Yukari0201 <130890393+Yukari0201@users.noreply.github.com>
2026-04-27 15:51:07 +0800

    fix: update script

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ echo "--- 正在更新 mw2fcitx ---"
 pipx install mw2fcitx[opencc] 2>/dev/null || pipx upgrade mw2fcitx[opencc]
 
 DICTS=(
-    "utils/yuanshen_dict.py"
+    "utils/genshin_dict.py"
     "utils/honkai3rd_dict.py"
     "utils/starrail_dict.py"
     "utils/zenlesszonezero_dict.py"
